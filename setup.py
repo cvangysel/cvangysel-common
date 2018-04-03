60dfae6a596254ca1032682829ebf7ff36cec67e
Christophe Van Gysel <cvangysel@uva.nl>
2018-04-03 14:56:51 +0200

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(name='cvangysel',
+      version='0.1',
+      description='Cross-project and reusable code',
+      author='Christophe Van Gysel',
+      author_email='cvangysel@uva.nl',
+      packages=['cvangysel'],
+      package_dir={'cvangysel': 'py/cvangysel'},
+      python_requires='>=3',
+      url='https://github.com/cvangysel/cvangysel-common',
+      download_url='https://github.com/cvangysel/cvangysel-common/tarball/0.1',
+      keywords=['tools', 'reusable'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Intended Audience :: Science/Research',
+          'Operating System :: POSIX :: Linux',
+      ])
